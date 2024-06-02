a1a092dc9b9140b8105c8a8596d9fdcd370ab4fc
Asus <git21cs100-t@students.git.edu>
2024-06-02 12:46:09 +0530

    First Commit

--- src/main/java/com/cricket/apis/CricInfoApplication.java
+++ src/main/java/com/cricket/apis/CricInfoApplication.java
@@ -2,6 +2,10 @@ package com.cricket.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CricInfoApplication {
@@ -11,3 +15,4 @@ public class CricInfoApplication {
 	}
 
 }
+
--- src/main/java/com/cricket/apis/controllers/MatchController.java
+++ src/main/java/com/cricket/apis/controllers/MatchController.java
@@ -4,6 +4,7 @@ import com.cricket.apis.entities.Match;
 import com.cricket.apis.services.MatchService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +19,17 @@ public class MatchController {
     public MatchController(MatchService matchService){
         this.matchService = matchService;
     }
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/live")
     public ResponseEntity<List<Match>> getLiveMatches(){
         return new ResponseEntity<>(this.matchService.getLiveMatches(), HttpStatus.OK);
     }
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<List<Match>> getAllMatches(){
         return new ResponseEntity<>(this.matchService.getAllMatches(),HttpStatus.OK);
     }
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/point-table")
     public ResponseEntity<?> getPointTable(){
         return new ResponseEntity<>(this.matchService.getPointTable(),HttpStatus.OK);
